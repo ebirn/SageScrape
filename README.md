4605525d72096b5e1c7062a56ac69c0349137b4b
Erich Birngruber <ebirn@outdated.at>
2016-01-07 12:51:54 +0100

    restructure to make module installable and usable

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Nothing there yet
+
---
+++ sc_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from datetime import date, time
+from sagescrape.dpw.timemanagement import TimeManagement
+
+if __name__ == "__main__":
+    # datetimel.time() gives the current time
+    my_enter = time(9,23)
+    my_exit = time(17,42)
+
+    tm = TimeManagement()
+    tm.launch()
+    tm.fill_times(date.today(), my_enter, my_exit)
+
+    tm.shutdown()
+
